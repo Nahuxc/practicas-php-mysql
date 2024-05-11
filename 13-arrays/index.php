023891d23524ecec7accf9382a5b809933c7355a
Nahuel <coronel98nahuel@gmail.com>
2024-05-11 12:02:37 -0300

    update arrays

---
+++ 13-arrays/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ARRAYS:
+un array es una coleccion de datos en donde podemos meterle mucha cantidad de variables o datos. bajo un unico nombre y para acceder a esos valores podemos usar un indice numero o alfanumerico
+
+
+*/
+
+
+$peliculas = array("spiderman", "mario bros", "sherlok");
+
+echo var_dump($peliculas); /* debugeamos para ver que tiene dentro */
+
+echo $peliculas[0]; /* con los corchetes y el indice podemos usar o ver cada variable */
+
+
+$cantantes = ["bad bunny", "2pac", "jennifer lopez"]; /* otra forma de definirlo es con los corchetes */
+
+echo $cantantes[1]
+
+
+
+?>
