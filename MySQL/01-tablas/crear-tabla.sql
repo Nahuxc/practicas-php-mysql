0b795918d67732edb1bfd554967c66661f59f62a
Nahuel <coronel98nahuel@gmail.com>
2024-05-20 19:07:30 -0300

    update mysql part 2

--- MySQL/01-tablas/crear-tabla.sql
+++ MySQL/01-tablas/crear-tabla.sql
@@ -8,8 +8,10 @@ create table
 
 show tables
 
-desc "nombre de la tabla"
+desc "nombre de la tabla para ver sus propiedades"
 
+
+drop "nombre de la tabla para eliminar"
 
 
 
@@ -52,11 +54,26 @@ desc "nombre de la tabla"
 
  CREATE TABLE usuarios(
     /* indicamos los campos que va a tener la tabla */
-    id          INT(11),
-    name        VARCHAR(100),
+    id          INT(11) auto_increment not null,
+    name        VARCHAR(100) not null,
     surname     VARCHAR(255),
-    email       VARCHAR(100),
-    password    VARCHAR(255)
+    email       VARCHAR(100) not null,
+    password    VARCHAR(255) not null
+    CONSTRAINT pk_usuarios PRIMARY KEY(id)
+
+   /* restricciones de integridad 
+
+    - not null, significa que el campo nunca puede ser nulo
+
+    - Null, podemos indicar que el valor si puede ser nulo.
+
+    - default "hola que tal", esto hace que por defecto el campo seleccionado tenga ese valor.
+
+    - auto_increment esto indica que el campo va a ser auto incrementable que solo va a poder ser usado para las claves primarias.
+
+   - CONSTRAINT pk_usuarios PRIMARY KEY(id) esto le va a dar una clave primaria de identificador, lo cual luego nos va ayudar para identificarlos.
+
+    */
 
 
     /* indicando el tipo de dato y su maxima cantidad de caracteres estariamos creando una tabla con sus respectiva propiedades */
