218edc2b6f20a6966c2ce8c79b2fa65fe9166dcb
Nahuel <coronel98nahuel@gmail.com>
2024-05-24 22:11:02 -0300

    update mysql part 4

---
+++ MySQL/04-DML/insert.sql
@@ -0,0 +1,5 @@
+# INSERTAR NUEVOS REGISTROS #
+
+# insert into sirve para insertar datos dentro de mi tabla,que le vamos a pasar por un VALUE() en el cual hay que integrarles los datos a guardar dependiendo, la cantidad y los campos que tengamos para completar #
+
+INSERT INTO users VALUES(null, "Jorge", "Gomez", "example@gmail.com", "124568", "24-05-2024");
