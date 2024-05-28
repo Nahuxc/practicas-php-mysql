39ed32ace9999916ba32b83da717114cd25b79ba
Nahuel <coronel98nahuel@gmail.com>
2024-05-28 12:55:22 -0300

    update mysql part 4

--- MySQL/04-DML/insert.sql
+++ MySQL/04-DML/insert.sql
@@ -2,4 +2,13 @@
 
 # insert into sirve para insertar datos dentro de mi tabla,que le vamos a pasar por un VALUE() en el cual hay que integrarles los datos a guardar dependiendo, la cantidad y los campos que tengamos para completar #
 
-INSERT INTO users VALUES(null, "Jorge", "Gomez", "example@gmail.com", "124568", "24-05-2024");
+
+/* esto hace que saque todos los registros de la tabla users */
+SELECT * FROM users;
+
+
+/* de esta manera insertamos usuarios */
+INSERT INTO users VALUES(null, "Jorge", "Gomez", "example@gmail.com", "124568", "2024-05-28");
+
+
+
