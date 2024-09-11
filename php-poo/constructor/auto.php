a954e2bfd519c8f4a36fba0f6938be819a6f9d7f
Nahuel <coronel98nahuel@gmail.com>
2024-09-11 20:02:05 -0300

    update php-poo constructor

--- php-poo/constructor/auto.php
+++ php-poo/constructor/auto.php
@@ -3,15 +3,24 @@
 
 class Auto{
 
-    //atributos o propiedades (variables)
+    //atributos o propiedades (variables) * atributo public podemos accder a el desde cualquier lugar
     public $color , $marca, $modelo, $velocidad;
 
+    //atributo protected podemos acceder a ellos desde la clase que los define y las clases que herenden la misma clase
+    protected $precio;
+
+    // atributo private solo podemos acceder desde la clase que lo define
+    private $vendedor;
+
+
     /* definir constructor */
-    public function __construct($color , $marca, $modelo, $velocidad){
+    public function __construct($color , $marca, $modelo, $velocidad, $precio, $vendedor){
         $this->color = $color;
         $this->marca = $marca;
         $this->modelo = $modelo;
         $this->velocidad = $velocidad;
+        $this->precio = $precio;
+        $this->vendedor = $vendedor;
     }
 
 
@@ -19,6 +28,11 @@ class Auto{
     public function arrancar(){
         echo "<h1>El auto esta encendido</h1>";
     }
+
+    public function getVendedor(){
+        return $this->vendedor;
+    }
+
 
     public function getColor(){
         return $this->color;
@@ -28,6 +42,15 @@ class Auto{
         return $this->color = $color;
 
     }
+
+    public function getPrecio(){
+        return $this->precio;
+    }
+
+    public function setPrecio($precio){
+        return $this->precio = $precio;
+    }
+
 
     public function acelerar(){
         $this->velocidad++;
@@ -39,6 +62,22 @@ class Auto{
 
     public function getVelocidad(){
         return $this->velocidad;
+    }
+
+
+    /* podemos indicar el tipo de dato que vamos a pasarle, este caso le pasamos de la clase auto */
+    public function getDatosAuto(auto $obj){
+
+        if(is_object($obj)){
+            $data = "<h1>Modelo: $obj->modelo </h1>";
+            $data .= "Marca: $obj->marca "."<br>";
+            $data .= "precio: $obj->precio ";
+        }else{
+            $data = "este dato esta mal";
+        }
+        
+        return $data;
+
     }
 
 
--- php-poo/constructor/index.php
+++ php-poo/constructor/index.php
@@ -3,13 +3,25 @@
 require_once "auto.php";
 
 
-$auto = new Auto("azul", "renault", "modelo 19", 300);
-$auto1 = new Auto("amarillo", "for", "eco", 230);
-$auto2 = new Auto("rojo", "ferrari", "tesla", 500);
+$auto = new Auto("azul", "renault", "modelo 19", 300, 13500, "martinez");
+$auto1 = new Auto("amarillo", "ford", "eco", 230, 3400, "juan");
+$auto2 = new Auto("rojo", "ferrari", "tesla", 500, 55400, "jose");
 
-var_dump($auto);
+//intentar modificar el atributto protected
+/* $auto->precio = 500;  nos da error porque no se puede */
+
+$auto->setPrecio(500023); /* en este caso como estamos modificando atraves de la clase si nos permite cambiarle el precio */
+
+echo $auto->getDatosAuto($auto2);
+
+/* con una funcion nos permite acceder al dato, sino no se puede, ya que tiene que ser si o si dentro de la clase */
+/* var_dump($auto->getPrecio());
+var_dump($auto->getVendedor()); */
+
+
+/* var_dump($auto);
 var_dump($auto1);
-var_dump($auto2);
+var_dump($auto2); */
 
 
 ?>
